bbddd85ecb3882c9e0c656c79eaafe4174c57f59
Tournier Quentin <qtournier@iutbayonne.univ-pau.fr>
2021-11-26 09:22:59 +0100

    Ajout de nouvelles fonction pour ajouter des nouvelles pages

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -9,12 +9,43 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStageController extends AbstractController
 {
     /**
-     * @Route("/pro/stage", name="pro_stage")
+     * @Route("/", name="pro_stage_accueil")
      */
     public function index(): Response
     {
         return $this->render('pro_stage/index.html.twig', [
-            'controller_name' => 'ProStageController',
+            'controller_name' => 'Bienvenue sur la page d\'accueil de Prostages',
         ]);
     }
+
+    /**
+     * @Route("/entreprises", name="pro_stage_entreprises")
+     */
+    public function indexEntreprise(): Response
+    {
+        return $this->render('pro_stage/index.html.twig', [
+            'controller_name' => 'Cette page affichera la liste des entreprises proposant un stage',
+        ]);
+    }
+
+    /**
+     * @Route("/formations", name="pro_stage_formations")
+     */
+    public function indexFormations(): Response
+    {
+        return $this->render('pro_stage/index.html.twig', [
+            'controller_name' => 'Cette page affichera la liste des formations de l\'IUT',
+        ]);
+    }
+
+    /**
+     * @Route("/stages/$id", name="pro_stage_stage_id")
+     */
+    public function indexStagesId(): Response
+    {
+        return $this->render('pro_stage/index.html.twig', [
+            'controller_name' => 'Cette page affichera le descriptif du stage ayant pour identifiant $id',
+        ]);
+    }
+    
 }
