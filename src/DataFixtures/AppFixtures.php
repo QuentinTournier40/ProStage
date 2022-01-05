702e36072d2066d42b73c318703d4832b7eeda90
Tournier Quentin <qtournier@iutbayonne.univ-pau.fr>
2022-01-05 12:29:30 +0100

    Création du jeu de données final, avec 15 entreprises qui proposent 3 stages chacune, presenté à 4 formations.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,8 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Entreprise;
+use App\Entity\Stage;
+use App\Entity\Formation;
 
 class AppFixtures extends Fixture
 {
@@ -12,14 +14,50 @@ class AppFixtures extends Fixture
     {
         $faker = \Faker\Factory::create('fr_FR');
 
-        $entreprise = new Entreprise();
-        $entreprise->setNom("Safran");
-        $entreprise->setAdresse($faker->realText($maxNbChars = 40, $indexSize = 2));
-        $entreprise->setActivite("aeronautique");
-        $entreprise->setLienSite("safran.com");
-        //$entreprise->addStage();
+        // Création des formations
+        $dutInfo = new Formation();
+        $dutInfo->setTitre("DUT Informatique");
 
-        $manager->persist($entreprise);
+        $lpProg = new Formation();
+        $lpProg->setTitre("Licence programmation avancée");
+
+        $lpMetierNum = new Formation();
+        $lpMetierNum->setTitre("Licence des metiers du numerique");
+
+        $dutGea = new Formation();
+        $dutGea->setTitre("DUT Gestion des entreprises et des administrations");
+
+        $tableauTypesFormations = array($dutInfo, $lpProg, $lpMetierNum, $dutGea);
+
+        foreach ($tableauTypesFormations as $typeFormation){
+            $manager->persist($typeFormation);
+        }
+
+        // Création des entreprises et de ses stages
+
+        for($i = 0; $i < 15 ; $i++){ 
+
+            //Création de l'entreprise
+            $entreprise = new Entreprise();
+            $entreprise->setNom($faker->company);
+            $entreprise->setAdresse($faker->address);
+            $entreprise->setActivite("aeronautique");
+            $entreprise->setLienSite($faker->companyEmail);
+
+            for($j = 0; $j < 3; $j++){
+                $stage = new Stage();
+                $stage->setTitre($faker->realText($maxNbChars = 200, $indexSize = 2));
+                $stage->setMission($faker->realText($maxNbChars = 600, $indexSize = 2));
+                $stage->setEmail($faker->companyEmail);
+                $stage->setEntreprise($entreprise);
+                $formationAChoisir = $faker->numberBetween($min = 0, $max = 3);
+                $stage->addTypeFormation($tableauTypesFormations[$formationAChoisir]);
+
+                $manager->persist($stage);
+            }
+
+            $manager->persist($entreprise);
+        }
         $manager->flush();
     }
 }
